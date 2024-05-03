787579e8ea374814999c77ea5f75c31bd93b0f22
lenovo <lenovo@LAPTOP-BLNTEBH7>
2024-05-03 11:51:20 +0530

    Add project files.

---
+++ Controllers/DataDisplayController.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace MyFirstApplication.Controllers
+{
+    public class DataDisplayController : Controller
+    {
+        // GET: DataDisplay
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public JsonResult GetData()
+        {
+            MyEmployeeDataEntities entities = new MyEmployeeDataEntities();
+
+            return Json(new { data = entities.EMP_Example.ToList() }, JsonRequestBehavior.AllowGet);
+
+        }
+
+    }
+}
